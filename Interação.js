c7434b6c927adcf1aec1d6653d3d76d6deb11054
MineMe22 <ERICKDASILVA241@GMAIL.COM>
2025-07-06 23:20:07 -0300

    interação do.js

---
+++ Interação.js
@@ -0,0 +1,4 @@
+document.getElementById('mostrarMensagemBtn').addEventListener('click', function () {
+    const mensagemDiv = document.getElementById('mensagem');
+    mensagemDiv.textContent = "Oii, caso queira conversar, me chama no discord: erickwq_";
+});
